d81368750ccc08b91010dc569e57a03fa99c782a
Тимур <kaftaranov@rambler.ru>
2022-04-25 21:19:23 +0600

    HomeWork_Cycles 3rd commit. Problem in the 3rd task with cycle and outcome.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
+        //Задание №1
+        short i = 1;
+        while (i <= 10) {
+    System.out.print(i+" ");
+    i+=1;
+        }
+System.out.println();
+for (i = 10; i >= 1; i--) {
+    System.out.print(i+" ");
+}
+System.out.println();
+System.out.println("------------------------------------");
 
+        //Задание №2
+//Пятница - 3 число.
+
+    for (short Friday = 6; Friday <=31; Friday +=7) {
+        if (Friday <= 31) {System.out.println("Today is Friday, "+Friday+"-th. Please prepare the report."); }
+        }
+        System.out.println("---------------------------------------");
+
+        //Задание №3
+    int CurrentYear = 2122;
+        System.out.print("Last years of Halley Comet were: ");
+    for (int CometYear = 0; CometYear <= CurrentYear + 100; CometYear += 79) {
+        if (CometYear >= CurrentYear - 200 && CometYear < CurrentYear) {System.out.print(CometYear+"; ");}
+        if (CometYear > CurrentYear) {System.out.println(); System.out.println("Next Halley Comet year is "+CometYear+".");}
+    }
     }
 }
